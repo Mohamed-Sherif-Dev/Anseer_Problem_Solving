8e06a62d794ddcc738d3f7422f6909ecb4057735
omr2023 <momoaa792@gmail.com>
2026-01-18 09:26:18 +0200

    first commit

---
+++ teast.js
@@ -0,0 +1,23 @@
+
+function evenOdd(value){
+    if(value % 2 == 0){
+        return "even";
+    } else{
+        return "odd";
+    }
+}
+
+console.log(evenOdd(5));
+
+
+function evenOdd1(value){
+    if( value % 2 !== 0){
+        return "odd";
+    } else{
+        return "even";
+    }
+}
+
+
+console.log(evenOdd1(6));
+
